8d04b28b7cd02db49339de5802b4849823b23662
Sergei Efimov <efimovsergeiv@gmail.com>
2023-05-12 13:41:59 +0300

    add:

---
+++ dapp/storage/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from storage.models import ProjectArchiveModel, FileArchiveModel
+
+class FileArchiveAdmin(admin.TabularInline):
+    model = FileArchiveModel
+    readonly_fields = ('created_date', 'md5')
+
+    fieldsets = (
+        (None, {'fields': (('md5', 'file', 'created_date'),)}),
+    )
+    extra = 0
+
+class ProjectArchiveAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_date')
+    list_display_links = ('id', 'name', 'created_date')
+    readonly_fields = ('created_date', )
+    inlines = (FileArchiveAdmin,)
+
+
+admin.site.register(ProjectArchiveModel, ProjectArchiveAdmin)
---
+++ dapp/storage/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+
+
+class ProjectArchiveModel(models.Model):
+    """ Модели архивы """
+
+    name = models.CharField(verbose_name="Название", max_length=120)
+    description = models.TextField(verbose_name="Описание", max_length=5000,default="Не описано")
+    created_date = models.DateTimeField(verbose_name="Дата создания", default=timezone.now)
+
+    class Meta:
+        verbose_name = "Модель"
+        verbose_name_plural = "Модели"
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class FileArchiveModel(models.Model):
+    """ Файлы """
+
+    project = models.ForeignKey(ProjectArchiveModel, verbose_name="Проект", on_delete=models.CASCADE)
+    md5 = models.CharField(verbose_name="MD5 сумма",max_length=100, null=True, blank=True)
+    file = models.FileField(verbose_name="Архив", upload_to="img/c/doc/")
+    created_date = models.DateTimeField(verbose_name="Дата создания", default=timezone.now)
+
+    class Meta:
+        verbose_name = "Архив"
+        verbose_name_plural = "Архивы"
+
+    def __str__(self) -> str:
+        return self.file.name
