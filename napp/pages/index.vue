eea38dd41300b015fca4a15f33fee9d42ae68ea5
Sergei Efimov <efimovsergeiv@gmail.com>
2023-05-16 08:40:49 +0300

    add:

--- napp/pages/index.vue
+++ napp/pages/index.vue
@@ -4,7 +4,7 @@
     <div class="relative">
       <div class="container mx-auto py-2 px-4">
         <div id="create-project" class="absolute z-10">
-          <div class="bg-sky-900 w-[1000px] px-4 py-4 rounded-lg">
+          <div class="bg-sky-900 w-[600px] px-4 py-4 rounded-lg">
             <div class="">
               <label for="message" class="block mt-2 mb-1 text-xs font-medium text-gray-100">Название проекта: {{ name }}</label>
                 <input id="text" v-model="name" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="МТВ-8000 ExpSuperVOC">
@@ -14,42 +14,37 @@
               <textarea id="message" v-model="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-sm border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Опишите проект..."></textarea>
             </div>
 
+            <div class="flex items-start justify-between">
+              <div class="">
+                <form  class="" @submit.prevent="uploadFile">
+                  <input
+                    id="file" type="file"
+                    class="block w-full text-sm text-gray-100
+                    file:mr-4 file:py-2 file:px-4
+                    file:rounded-full file:border-0
+                    file:text-sm file:font-semibold
+                    file:bg-white file:text-sky-800
+                    hover:file:bg-white
+                  " @change="onFileChange"/>
 
-            <form  class="flex items-center space-x-6" @submit.prevent="uploadFile">
-              <label class="block">
-                <input
-                  id="file" type="file"
-                  class="block w-full text-sm text-slate-500
-                  file:mr-4 file:py-2 file:px-4
-                  file:rounded-full file:border-0
-                  file:text-sm file:font-semibold
-                  file:bg-white file:text-sky-700
-                  hover:file:bg-white
-                " @change="onFileChange"/>
-              </label>
-
-              <div v-if="uploadProgress !== null">
-                <span class="text-white">Upload Progress: {{ uploadProgress }}% </span>
-                <progress class="h-4 text-green-400 border border-white rounded-sm" :value="uploadProgress" max="100">{{ uploadProgress }}%</progress>
-              </div>
-              
-              <button class="" type="submit">Загрузить файл</button>
-
-            </form>
-
-            <!-- <div class="flex items-center justify-between">
-              <div>
-                <form @submit.prevent="uploadFile">
-                  <div>
-                    <label for="file">File:</label>
-                    <input id="file" type="file" @change="onFileChange" />
+                  <div class="mt-4">
+                    <button id="file" class="px-6 py-2 bg-white rounded-full text-sm font-semibold text-sky-900" type="submit">Создать</button>
                   </div>
-                  
-
 
                 </form>
               </div>
-            </div> -->
+              <div class="">
+                <div class="text-center">
+                  <span class="text-white text-xs font-semibold w-full text-center"> {{ uploadProgress }}% </span>
+                </div>                
+                <div class="">
+                  <progress class="h-4 text-green-400 border border-white rounded-sm" :value="uploadProgress" max="100">{{ uploadProgress }}%</progress>
+                </div>
+              </div>
+            </div>
+
+
+
           </div>
         </div>
       </div>
@@ -73,7 +68,7 @@ export default {
       filename: null,
 
       file: null,
-      uploadProgress: null,
+      uploadProgress: 0,
     }
   },
   methods: {
