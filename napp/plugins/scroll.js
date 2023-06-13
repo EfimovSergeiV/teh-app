b6014e93e1f048c70ef19131f9927b8e4ad11216
Sergei Efimov <efimovsergeiv@gmail.com>
2023-06-13 12:41:43 +0300

    add:

--- napp/plugins/scroll.js
+++ napp/plugins/scroll.js
@@ -1,7 +1,7 @@
 export default function({ app }) {
   if (process.client) {
     window.addEventListener('scroll', () => {
-      const scrollTop = (window.pageYOffset || document.documentElement.scrollTop)/200;
+      const scrollTop = (window.pageYOffset || document.documentElement.scrollTop)/300;
       const background = document.querySelector('#background-page');
       if (background) {
         background.style.backgroundPositionY = `${6 + scrollTop}rem`;
