bd50b1c6228e789b9cd91fb6d8b37107a84a24af
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-06-28 13:30:58 +0300

    add:

--- dapp/main/settings.py
+++ dapp/main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#%$x3lr=75xcn!wo_9x_-@6gus9bcv83d&xlbe5i^ga2e46em3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*',
